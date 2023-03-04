c5b3fa1c2f1f21d6cbcb4d5341f334a91c909c0a
IAmPavelDev <179werpavel@protonmail.com>
2023-03-05 00:24:29 +0200

    About page done

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,28 +1,56 @@
 import { Slide } from "features/AboutSlide";
-import { useEffect, useRef } from "react";
+import { CSSProperties, useEffect, useRef } from "react";
 import style from "./About.module.scss";
+
+import { ReactComponent as MobX } from "./TecIcons/Mobx-icon.svg";
+import { ReactComponent as Mongodb } from "./TecIcons/Mongodb-icon.svg";
+import { ReactComponent as MUI } from "./TecIcons/MUI-icon.svg";
+import { ReactComponent as Nest } from "./TecIcons/Nest-icon.svg";
+import { ReactComponent as React } from "./TecIcons/React-icon.svg";
+import { ReactComponent as Redux } from "./TecIcons/Redux-icon.svg";
+import { ReactComponent as Sass } from "./TecIcons/Sass-icon.svg";
+import { ReactComponent as Tailwind } from "./TecIcons/Tailwind-icon.svg";
+import { ReactComponent as Tux } from "./TecIcons/Tux-icon.svg";
+import { ReactComponent as Vite } from "./TecIcons/Vite-icon.svg";
+import { ReactComponent as Webpack } from "./TecIcons/Webpack-icon.svg";
 
 export const About = () => {
   const place = useRef<HTMLDivElement>(null);
   const xp = useRef<HTMLDivElement>(null);
+  const tec = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (xp.current && place.current) {
-        if (window.scrollY < 990) {
-          place.current.style.transform =
-            "scale(" + (1000 - window.scrollY) / 1000 + ")";
-        }
-        if (window.scrollY < 1200) {
-          xp.current.style.transform = `translateY(${
-            (1200 - window.scrollY) / 10
-          }%) scale(${-0.000209 * window.scrollY + 1.25})`;
-        } else if (window.scrollY > 1250 && window.scrollY < 2240) {
-          xp.current.style.transform = `translateY(0%) scale(${
-            (2250 - window.scrollY) / 1000
-          })`;
-        } else {
-          xp.current.style.transform = "translateY(0%) scale(1)";
+      if (xp.current && place.current && tec.current) {
+        const Y = window.scrollY;
+        switch (true) {
+          case Y < 1200: {
+            if (Y < 990)
+              place.current.style.transform =
+                "scale(" + (1000 - window.scrollY) / 1000 + ")";
+            xp.current.style.transform = `translateY(${
+              (1200 - window.scrollY) / 10
+            }%) scale(${-0.000209 * window.scrollY + 1.25})`;
+            break;
+          }
+          case Y >= 1200 && Y <= 1250: {
+            xp.current.style.transform = "translateY(0%) scale(1)";
+            break;
+          }
+          case Y > 1250 && Y < 2500: {
+            xp.current.style.transform = `translateY(0%) scale(${
+              (2500 - window.scrollY) / 1250
+            })`;
+            tec.current.style.transform = `translateY(${
+              (2500 - window.scrollY) / 10
+            }%) scale(${-0.0002 * window.scrollY + 1.5})`;
+            break;
+          }
+          case Y > 2500: {
+            xp.current.style.transform = "translateY(0%) scale(0.094)";
+            tec.current.style.transform = `translateY(0%) scale(1)`;
+            break;
+          }
         }
       }
     });
@@ -44,7 +72,81 @@ export const About = () => {
             videoLink="/assets/gosurf.mp4"
           />
         </div>
-        <div className={style.slides__tecs}></div>
+        <div ref={tec} className={style.slides__tecs}>
+          <div className={style.sliderContainer}>
+            <div className={style.tecs__props}>
+              <div
+                style={{ "--idx": 1 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <MobX />
+              </div>
+              <div
+                style={{ "--idx": 2 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <React />
+              </div>
+              <div
+                style={{ "--idx": 3 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Redux />
+              </div>
+              <div
+                style={{ "--idx": 4 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Webpack />
+              </div>
+              <div
+                style={{ "--idx": 5 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Tux />
+              </div>
+              <div
+                style={{ "--idx": 6 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Vite />
+              </div>
+              <div
+                style={{ "--idx": 7 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <MUI />
+              </div>
+              <div
+                style={{ "--idx": 8 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Nest />
+              </div>
+              <div
+                style={{ "--idx": 9 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Sass />
+              </div>
+              <div
+                style={{ "--idx": 10 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Tailwind />
+              </div>
+              <div
+                style={{ "--idx": 11 } as CSSProperties}
+                className={style.props__prop}
+              >
+                <Mongodb />
+              </div>
+            </div>
+          </div>
+          <div className={style.info}>
+            Here you can see every technology I have experience in.
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import style from "./Home.module.scss";
 
 import { PostList } from "features/PostList";
-import { Head } from "features/Head";
 import { SliderOfRecentPosts } from "features/SliderOfRecentPosts";
 
-import { store } from "shared/store";
-
 export const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
   return (
     <div className={style.wrapper}>
       <div className={style.wrapper__content}>
